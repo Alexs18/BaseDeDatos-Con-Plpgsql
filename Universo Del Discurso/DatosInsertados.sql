bc0dcf2ca5784df868768478ee58cd983bc17b9c
Alexs18 <50090511+Alexs18@users.noreply.github.com>
2020-12-18 00:52:13 -0500

    Add files via upload

---
+++ Universo Del Discurso/DatosInsertados.sql
@@ -0,0 +1,93 @@
+﻿insert into profesion (descripcion_profesion) values ('Ingeniero en sistemas');
+insert into profesion (descripcion_profesion) values ('Ingeniero mecanico');
+insert into profesion (descripcion_profesion) values ('Ingeniero electrico');
+insert into profesion (descripcion_profesion) values ('Analista de sistemas');
+insert into profesion (descripcion_profesion) values ('Economista');
+
+insert into docente (idprofesion, nombre_docente, apellido_docente, cedula_docente,telefono_docente, correo_docente) values
+		('1','Jhon Ariel', 'Cevallos Ortiz', '123456789', 098735363736, 'JHONCO@LIVE.ULEAM.EDU.EC');
+insert into docente (idprofesion, nombre_docente, apellido_docente, cedula_docente,telefono_docente, correo_docente) values
+		('3', 'Roberth Alexander', 'Moreira Centeno', '142363738', 0985646664, 'ROBERTHCO@LIVE.ULEAM.EDU.EC');
+insert into docente (idprofesion, nombre_docente, apellido_docente,cedula_docente,telefono_docente, correo_docente) values
+		('2', 'Adriana Macias', 'CEVALLOS ORTIZ', '123456789', 098735363736, 'JHONCO@LIVE.ULEAM.EDU.EC');
+insert into docente (idprofesion,nombre_docente, apellido_docente,cedula_docente,telefono_docente, correo_docente) values
+		('5', 'Walter Alfredo', 'Rivas Peñafiel', '123456789', 098735363736, 'WalterCO@LIVE.ULEAM.EDU.EC');
+insert into docente (idprofesion, nombre_docente, apellido_docente,cedula_docente,telefono_docente, correo_docente) values
+		('1', 'Marina Alexandra', 'CEVALLOS Cedeño', '123456789', 098735363736, 'MarinaCO@LIVE.ULEAM.EDU.EC');
+
+insert into nivel (descripcion_nivel) values ('1ero');
+insert into nivel (descripcion_nivel) values ('2do');
+insert into nivel (descripcion_nivel) values ('3ero');
+insert into nivel (descripcion_nivel) values ('4to');
+insert into nivel (descripcion_nivel) values ('5to');
+
+
+insert into Facultad (descripcion_facultad) values ('Facultad de ciencias Informaticas');
+insert into Facultad (descripcion_facultad) values ('Facultad de ingenieria');
+
+insert into carrera (idfacultad, descripcion_carrera) values ('1', 'Ingenieria en sistemas');
+insert into carrera (idfacultad, descripcion_carrera) values ('1', 'Ingenieria en TI');
+insert into carrera (idfacultad, descripcion_carrera) values ('2', 'Arquitectura');
+insert into carrera (idfacultad, descripcion_carrera) values ('2', 'Electronica');
+
+
+insert into estudiante (idcarrera, nombre_estudiante, apellidos_estudiante, telefono_estudiante, cedula_estudiante, correo_estudiante)
+values ('1', 'Alexis Ricardo', 'López Mero', 0973645373, '134245533', 'Alexis18@gmail.com' );
+insert into estudiante (idcarrera, nombre_estudiante, apellidos_estudiante, telefono_estudiante, cedula_estudiante, correo_estudiante)
+values ('3', 'Alexander kevin', 'Espinoza Delgado', 09736456353, '136343648', 'Alexander19@gmail.com' );
+insert into estudiante (idcarrera, nombre_estudiante, apellidos_estudiante, telefono_estudiante, cedula_estudiante, correo_estudiante)
+values ('2', 'Karen Estefania', 'Santana Chavez', 09735435373, '134634363', 'Karen20@gmail.com' );
+insert into estudiante (idcarrera, nombre_estudiante, apellidos_estudiante, telefono_estudiante, cedula_estudiante, correo_estudiante)
+values ('4', 'Leidy Nicole', 'Ponce Delgado', 0973454373, '134232533', 'Leidy21@gmail.com' );
+
+
+insert into asignatura (idnivel, descripcion_asignatura) values ('2', 'Algebra Lineal');
+insert into asignatura (idnivel, descripcion_asignatura) values ('3', 'Redes y tecnologia');
+insert into asignatura (idnivel, descripcion_asignatura) values ('1', 'Algoritmos');
+insert into asignatura (idnivel, descripcion_asignatura) values ('3', 'Economia basica');
+
+
+insert into inscripcion (idasignatura, idestudiante) values ('1', '1');
+insert into inscripcion (idasignatura, idestudiante) values ('2', '1');
+insert into inscripcion (idasignatura, idestudiante) values ('4', '4');
+insert into inscripcion (idasignatura, idestudiante) values ('1', '4');
+
+
+insert into carreradocente (iddocente, idcarrera) values ('1', '1');
+insert into carreradocente (iddocente, idcarrera) values ('3', '1');
+insert into carreradocente (iddocente, idcarrera) values ('4', '2');
+insert into carreradocente (iddocente, idcarrera) values ('5', '2');
+
+insert into impartirasignatura (iddocente, idasignatura) values ('1', '2');
+insert into impartirasignatura (iddocente, idasignatura) values ('1', '3');
+insert into impartirasignatura (iddocente, idasignatura) values ('4','4');
+insert into impartirasignatura (iddocente, idasignatura) values ('4','1');
+insert into impartirasignatura (iddocente, idasignatura) values ('2','3');
+insert into impartirasignatura (iddocente, idasignatura) values ('4','2');
+insert into impartirasignatura (iddocente, idasignatura) values ('3','3');
+
+
+
+insert into tutoria (idestudiante,iddocente, fecha_tutoria, horario_tutoria,medio_tutoria, enlace_tutoria) 
+values ('1', '2', '2020-12-21', '18:00:00','zoom', 'zomgdhgdhjk2537/25342638WH');
+insert into tutoria (idestudiante,iddocente, fecha_tutoria, horario_tutoria,medio_tutoria, enlace_tutoria) 
+values ('2', '3', '2020-12-19', '13:00:00','teams', 'teaomgdhgdhjeteg537/25342638WH');
+insert into tutoria (idestudiante,iddocente, fecha_tutoria, horario_tutoria,medio_tutoria, enlace_tutoria) 
+values ('1', '4', '2020-12-18', '10:00:00','skype', 'zomgdhgdhjk25reee25342638WH');
+
+
+insert into detalle_tutoria (idtutoria, tema_tutoria, horario_fin, recomendaciones_tutoria) 
+values ('1', 'Enrutamiento Dinamico', '19:00:00' ,'Leer los libros sobre enrutamiento colocados en el aula virtual');
+insert into detalle_tutoria (idtutoria, tema_tutoria, horario_fin, recomendaciones_tutoria) 
+values ('2', 'Los algoritmos en torno a la sociedad', '13:50:00' ,'Analizar los datos de nuestroe entorno en base a la abstraccion');
+insert into detalle_tutoria (idtutoria, tema_tutoria, horario_fin, recomendaciones_tutoria) 
+values ('3', 'La economia en los paises subdesarrollados', '13:50:00' ,'Investigar sobre los procesos economicos que manejan las potencias mundiales');
+
+
+
+
+
+
+
+
+
---
+++ Universo Del Discurso/FinalExposicion.sql
@@ -0,0 +1,167 @@
+﻿/*==============================================================*/
+/* Table: PROFESION                                             */
+/*==============================================================*/
+create table PROFESION (
+   IDPROFESION          SERIAL               not null,
+   DESCRIPCION_PROFESION CHAR(40)             not null,
+   constraint PK_PROFESION primary key (IDPROFESION)
+);
+
+/*==============================================================*/
+/*  Creacion de tablas                      			 */
+/*==============================================================*/
+/*  Tabla Docente                 			 */
+
+create table DOCENTE (
+   IDDOCENTE            SERIAL                not null,
+   IDPROFESION          INT4                 null,
+   NOMBRE_DOCENTE       CHAR(50)             not null,
+   APELLIDO_DOCENTE     CHAR(50)             not null,
+   CEDULA_DOCENTE       NUMERIC(11)          not null,
+   TELEFONO_DOCENTE     NUMERIC(13)          not null,
+   CORREO_DOCENTE       CHAR(50)             not null,
+   constraint PK_DOCENTE primary key (IDDOCENTE),
+   constraint FK_DOCENTE_PROFESION_PROFESIO foreign key (IDPROFESION)
+      references PROFESION (IDPROFESION)
+      on delete restrict on update restrict
+);
+/*==============================================================*/
+/* Table: FACULTAD                                              */
+/*==============================================================*/
+create table FACULTAD (
+   IDFACULTAD           SERIAL                not null,
+   DESCRIPCION_FACULTAD CHAR(40)             not null,
+   constraint PK_FACULTAD primary key (IDFACULTAD)
+);
+/*==============================================================*/
+/* Table: NIVEL                                                 */
+/*==============================================================*/
+create table NIVEL (
+   IDNIVEL              SERIAL                 not null,
+   DESCRIPCION_NIVEL    CHAR(10)             not null,
+   constraint PK_NIVEL primary key (IDNIVEL)
+);
+/*==============================================================*/
+/* Table: CARRERA                                               */
+/*==============================================================*/
+create table CARRERA (
+   IDCARRERA            SERIAL                 not null,
+   IDFACULTAD           INT4                 not null,
+   DESCRIPCION_CARRERA  CHAR(40)             not null,
+   constraint PK_CARRERA primary key (IDCARRERA),
+   constraint FK_CARRERA_POSEE_VAR_FACULTAD foreign key (IDFACULTAD)
+      references FACULTAD (IDFACULTAD)
+      on delete restrict on update restrict
+);
+/*==============================================================*/
+/* Table: ESTUDIANTE                                            */
+/*==============================================================*/
+create table ESTUDIANTE (
+   IDESTUDIANTE         SERIAL               not null,
+   IDCARRERA            INT4                 not null,
+   NOMBRE_ESTUDIANTE    CHAR(50)             not null,
+   APELLIDOS_ESTUDIANTE CHAR(50)             not null,
+   TELEFONO_ESTUDIANTE  NUMERIC(20)          not null,
+   CEDULA_ESTUDIANTE    NUMERIC(11)          not null,
+   CORREO_ESTUDIANTE    CHAR(50)             not null,
+   constraint PK_ESTUDIANTE primary key (IDESTUDIANTE),
+   constraint FK_ESTUDIAN_CUENTA_CO_CARRERA foreign key (IDCARRERA)
+      references CARRERA (IDCARRERA)
+      on delete restrict on update restrict
+   
+);
+/*==============================================================*/
+/* Table: ASIGNATURA                                            */
+/*==============================================================*/
+create table ASIGNATURA (
+   IDASIGNATURA         SERIAL                not null,
+   IDNIVEL              INT4                 not null,
+   DESCRIPCION_ASIGNATURA CHAR(40)             not null,
+   constraint PK_ASIGNATURA primary key (IDASIGNATURA),
+   constraint FK_ASIGNATU_TIENE_MUC_NIVEL foreign key (IDNIVEL)
+      references NIVEL (IDNIVEL)
+      on delete restrict on update restrict
+   
+);
+/*==============================================================*/
+/* Table: INSCRIPCION                                           */
+/*==============================================================*/
+create table INSCRIPCION (
+   IDINSCRIPCION        SERIAL                 not null,
+   IDASIGNATURA         INT4                 not null,
+   IDESTUDIANTE         INT4                 not null,
+   constraint PK_INSCRIPCION primary key (IDINSCRIPCION),
+   constraint FK_INSCRIPC_ES_INSCRI_ASIGNATU foreign key (IDASIGNATURA)
+      references ASIGNATURA (IDASIGNATURA)
+      on delete restrict on update restrict,
+      constraint FK_INSCRIPC_SE_INCRIB_ESTUDIAN foreign key (IDESTUDIANTE)
+      references ESTUDIANTE (IDESTUDIANTE)
+      on delete restrict on update restrict
+      
+);
+/*==============================================================*/
+/* Table: CARRERADOCENTE                                        */
+/*==============================================================*/
+create table CARRERADOCENTE (
+   IDCARRERA_DOCENTE    SERIAL                not null,
+   IDDOCENTE            INT4                 not null,
+   IDCARRERA            INT4                 not null,
+   constraint PK_CARRERADOCENTE primary key (IDCARRERA_DOCENTE),
+   constraint FK_CARRERAD_CUENTA_CO_CARRERA foreign key (IDCARRERA)
+      references CARRERA (IDCARRERA)
+      on delete restrict on update restrict,
+      constraint FK_CARRERAD_PUEDE_TEN_DOCENTE foreign key (IDDOCENTE)
+      references DOCENTE (IDDOCENTE)
+      on delete restrict on update restrict
+      
+
+);
+/*==============================================================*/
+/* Table: IMPARTIRASIGNATURA                                    */
+/*==============================================================*/
+create table IMPARTIRASIGNATURA (
+   IDIMPARTIR           SERIAL                not null,
+   IDDOCENTE            INT4                 not null,
+   IDASIGNATURA         INT4                 not null,
+   constraint PK_IMPARTIRASIGNATURA primary key (IDIMPARTIR),
+   constraint FK_IMPARTIR_ES_IMPART_ASIGNATU foreign key (IDASIGNATURA)
+      references ASIGNATURA (IDASIGNATURA)
+      on delete restrict on update restrict,
+      constraint FK_IMPARTIR_IMPARTE_M_DOCENTE foreign key (IDDOCENTE)
+      references DOCENTE (IDDOCENTE)
+      on delete restrict on update restrict
+);
+/*==============================================================*/
+/* Table: TUTORIA                                               */
+/*==============================================================*/
+create table TUTORIA (
+   IDTUTORIA            SERIAL                 not null,
+   IDESTUDIANTE         INT4                 not null,
+   IDDOCENTE            INT4                 not null,
+   FECHA_TUTORIA        DATE                 not null,
+   HORARIO_TUTORIA      TIME                 not null,
+   MEDIO_TUTORIA        CHAR(35)             not null,
+   ENLACE_TUTORIA       CHAR(500)            not null,
+   constraint PK_TUTORIA primary key (IDTUTORIA),
+   constraint FK_TUTORIA_DA_MUCHAS_DOCENTE foreign key (IDDOCENTE)
+      references DOCENTE (IDDOCENTE)
+      on delete restrict on update restrict,
+      constraint FK_TUTORIA_VE_MUCHAS_ESTUDIAN foreign key (IDESTUDIANTE)
+      references ESTUDIANTE (IDESTUDIANTE)
+      on delete restrict on update restrict
+
+);
+/*==============================================================*/
+/* Table: DETALLE_TUTORIA                                       */
+/*==============================================================*/
+create table DETALLE_TUTORIA (
+   IDDETALLE_TUTORIA    SERIAL              not null,
+   IDTUTORIA            INT4                 not null,
+   TEMA_TUTORIA         CHAR(300)            not null,
+   RECOMENDACIONES_TUTORIA CHAR(300)            not null,
+   constraint PK_DETALLE_TUTORIA primary key (IDDETALLE_TUTORIA),
+   constraint FK_DETALLE__POSEE_MUC_TUTORIA foreign key (IDTUTORIA)
+      references TUTORIA (IDTUTORIA)
+      on delete restrict on update restrict
+   
+);
